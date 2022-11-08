a165fc29dc101f0a4bc146da4ccf9f73a1ce10dc
emilianobrazzoli <emiliano.brazzoli@gmail.com>
2022-11-08 16:55:18 +0100

    new homepage

--- src/resource/js/home.js
+++ src/resource/js/home.js
@@ -2,14 +2,21 @@
 var hydra = new Hydra({ detectAudio: true, canvas: document.getElementById("hydra-canvas") });
 hydra.setResolution(1920, 1080);
 
-s0.initImage("./src/resource/img/alpha.png")
+s1.initImage("./src/resource/img/alpha.png")
 a.setCutoff(4)
 a.setSmooth(0.8)
-src(s0).scroll([-0.2,0.2].smooth(),[-0.2,0.2,0.1,-0.15].smooth())
+src(s1).invert(1)  
+  .rotate( () => time%360, () => Math.sin(time*0.1)*0.05 )
+  .scroll([-0.2,0.2].smooth(),[-0.2,0.2,0.1,-0.15].smooth())
 .scale(()=>1+a.fft[0])
-  .modulateHue(src(o0).sub(s0,1).scale(1.01),1)
-  .diff(osc(4,0.5,2).mask(shape(4,1,0.001)))
-  .out(o0)
+  .out(o1)
 
+
+src(o1)
+	.add(
+  		src(o0).scale(0.95) 
+  		.brightness(.15)
+  		,.7)
+	.out()
 
  
